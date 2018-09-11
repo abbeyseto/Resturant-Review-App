f3cf2007fd046ebaabcc2a025549936796c72202
Adenle Abiodun <adenleabbey@gmail.com>
2018-09-11 03:05:36 +0100

    UPDATE:
    * install to device prompt added

--- js/main.js
+++ js/main.js
@@ -223,3 +223,40 @@ if('serviceWorker' in navigator){
 }else{
     console.log( "Your browser is not compactible with Service Worker!");
 }
+
+let deferredPrompt;
+let btnAdd = document.createElement("div");
+btnAdd.innerHTML = "Add Resturant review to your home screen?";
+btnAdd.setAttribute('display', 'none');
+btnAdd.className = 'install-prompt';
+document.body.append(btnAdd);
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  app.logEvent('a2hs', 'installed');
+});
