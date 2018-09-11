92104dea445de91355319a0afe58b6a32fb42c37
Adenle Abiodun <adenleabbey@gmail.com>
2018-09-11 05:32:48 +0100

    Add files via upload
    Service worker added to this destination

---
+++ servw.js
@@ -0,0 +1,80 @@
+var staticCacheName = 'RestRevApp1';
+
+
+self.addEventListener('install', function (event) {
+
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/css/styles.css',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/data/restaurants.json',
+                '/index.html',
+                '/restaurant.html',
+                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+                'https://abbeyseto.github.io/Resturant-Review-App/data/restaurants.json'
+            ]);
+        }).then(console.log('Yay... cache is sucessful'))
+    );
+});
+
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('Rest') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
+// self.addEventListener('fetch', function (event) {
+
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(function (response) {
+//                 console.log('Found', event.request, 'in cache');
+//                 return response;
+//             })
+//             .catch(function (response) {
+//                 console.log('Cound not find', event.request, 'in cache, Fetching ...');
+//                 return fetch(event.request)
+//             })
+//             .then(function (response) {
+//                 const clonedResponse = response.clone();
+//                 caches.open(staticCacheName).then(function (cache) {
+//                     cache.put(event.request, clonedResponse);
+//                 })
+//                 return response;
+//             })
+//     )
+// });
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+return response || fetch(event.request);
+    })
+  );
+  });
